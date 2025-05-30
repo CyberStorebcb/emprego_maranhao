40bad77b18ef59797fa5f5ae8545b1e26ca0d7c0
CyberStorebcb <cyberstorebcb@gmail.com>
2025-05-30 13:52:19 -0300

    fix: update dev script to remove turbopack and clean up config options

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  // Se seu repositório não for raiz, adicione o basePath:
+  // basePath: '/NOME-DO-REPO',
+  // assetPrefix: '/NOME-DO-REPO',
 };
 
 export default nextConfig;
