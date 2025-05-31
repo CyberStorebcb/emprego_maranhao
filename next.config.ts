a3bbead1d9e23375a95cd3f9a6e45850f081b023
CyberStorebcb <cyberstorebcb@gmail.com>
2025-05-31 08:49:04 -0300

    fix: update next.config.ts and package.json for improved deployment configuration

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
-import type { NextConfig } from "next";
-
 /** @type {import('next').NextConfig} */
-const nextConfig: NextConfig = {
-  output: "export",
-  // Se seu repositório não for raiz, adicione o basePath:
-  // basePath: '/NOME-DO-REPO',
-  // assetPrefix: '/NOME-DO-REPO',
+const repo = 'emprego_maranhao'; // nome do seu repositório
+
+const nextConfig = {
+  output: 'export',
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
